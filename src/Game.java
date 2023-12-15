54a352924af4701cd4e2e22fb07cbe5447588a3d
MohamedMahrous87 <m.mahrous201187@gmail.com>
2023-12-15 22:02:58 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Game {
+
+	
+	
+	public Game() {
+		// TODO Auto-generated constructor stub
+		System.out.println("===================================================================================");
+		System.out.println("=============================  Wellcom to TIC TAC TOU =============================");
+		System.out.println("===================================================================================");
+		System.out.println("=====================Enter Players Names And Symbols( X  or O )====================");
+	}
+
+
+	
+
+	
+}
