39ee2c19324fe8e3f78ab91b6a2413235765fbf1
MohamedMahrous87 <m.mahrous201187@gmail.com>
2023-12-17 01:05:06 +0300

    edite3

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,20 @@
-
 import java.util.*;
+
 public class Game {
 
-	
-	
-	public Game() {
+		private static char[] posn = {'1','2','3','4','5','6','7','8','9'};
+		
+		//game constructor
+		public Game() {
 		System.out.println("===================================================================================");
 		System.out.println("=============================  Wellcom to TIC TAC TOU =============================");
 		System.out.println("===================================================================================");
 		System.out.println("=====================Enter Players Names And Symbols( X  or O )====================");
 	}
 
-	
-	  public static int menu() {
+		
+		//showing choices menu for players
+		public static int menu() {
 
 	        int selection = 0 ;
 	        String val;
@@ -41,5 +43,36 @@ public class Game {
 	        	return selection; 
 	        	
 	    }
-	
+
+		
+		//painting playground for all players
+		public static void paintBoard() {
+		  
+		  //  System.out.println( "\n\n" );
+			System.out.println(  "\n" );
+			System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  "\t\t" + posn [0] + "   | " +posn [1]+ "  | " +posn [2]);
+	        System.out.println(  " \t\t ___|____|___ " );
+	        System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  "\t\t" +posn [3]+ "   | " +posn [4]+ "  | " +posn [5]);
+	        System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  " \t\t ___|____|___ " );
+	        System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  "\t\t" +posn [6]+ "   | " +posn [7]+ "  | " +posn [8]);
+	        System.out.println(  " \t\t    |    |   " );
+	        System.out.println(  "\n" ); 
+	        
+
+	  }
+		
+		
+		//painting playground for all players
+		public static void move() {
+			Scanner move =  new Scanner(System.in);
+			System.out.print(  "press number to play (X) player >> " ); 
+			move.next();
+		}
+		}
+		
+		
 }
--- src/main.java
+++ src/main.java
@@ -12,7 +12,6 @@ public class main {
 		static Player p2 =  new Player();
 
 	
-	
 	public static void getPlayers() {
 		
 		//creating first player and reading data 
@@ -39,15 +38,31 @@ public class main {
 	
 	
 	public static void main(String[] args) {
-		int choice = 0;
-		boolean stat = false;
+	
 		//getting players data
-		//main m = new main();	
 		getPlayers();
-		//printPlayers();
-		
+	
 		//show selection menu
 		game.menu();
+		
+		//paint playground 
+		game.paintBoard();
+		
+		//get player move
+		
+		
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
